342f2b952fe087e0f5be4e2aede2250d1a5f1c3f
Dương Thành Thái <31364175+duongthanhthai@users.noreply.github.com>
2023-09-14 20:52:17 +0700

    Update flatsome.php

--- flatsome.php
+++ flatsome.php
@@ -75,9 +75,9 @@ function wupdates_add_id_YL6Wd( $ids = array() ) {
     return $ids;
 }
 add_filter( 'wupdates_gather_ids', 'wupdates_add_id_YL6Wd', 10, 1 );
-update_option( 'flatsome_wup_purchase_code', 'GWrxBEss-VqSg-cJbs-dVvg-QzLEDfLzzExZ' );
+update_option( 'flatsome_wup_purchase_code', '846b8d75-85b3-4b3e-976c-2e29d386339d' );
 update_option( 'flatsome_wup_supported_until', '14.07.2027' );
-update_option( 'flatsome_wup_buyer', 'Licensed' );
+update_option( 'flatsome_wup_buyer', 'THANHTHAI.ORG' );
 update_option( 'flatsome_wup_sold_at', time() );
 delete_option( 'flatsome_wup_errors', '' );
 delete_option( 'flatsome_wupdates', '');
